f12a1ef08ea3524eb9e96fc95497c43d8bea9edf
Salaudeen Muhammed-Fatiu Akanfe <salaudeenfatiu091@gmail.com>
2023-09-14 16:22:21 -0700

    Fourth commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
+
+int _putchar(char c)
+
+{
+	return write(1, &c, 1);
+}
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return 1; // The character is uppercase
+	}
+	else
+	{
+	return 0; // The character is not uppercase
+	}
+}
 
 int main(void)
 
@@ -11,12 +30,26 @@ int main(void)
 
 	if (_isupper(character))
 	{
-	printf("%c is uppercase\n", character);
+	_putchar(character);
+	_putchar(' ');
+	_putchar('i');
+	_putchar('s');
+	_putchar(' ');
+	_putchar('uppercase');
+	_putchar('\n');
 	}
 	else
 	{
-	printf("%c is not uppercase\n", character);
+	_putchar(character);
+	_putchar(' ');
+	_putchar('i');
+	_putchar('s');
+	_putchar(' ');
+	_putchar('not');
+	_putchar(' ');
+	_putchar('uppercase');
+	_putchar('\n');
 	}
 
-	return (0);
+	return 0;
 }

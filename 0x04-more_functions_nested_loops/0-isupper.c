3573964fcfaea06bcdff527c50ac0b5bd3548c8c
Salaudeen Muhammed-Fatiu Akanfe <salaudeenfatiu091@gmail.com>
2023-09-14 17:35:24 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,55 +1,16 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
 
-int _putchar(char c)
-
-{
-	return write(1, &c, 1);
-}
-
 int _isupper(int c)
-
 {
 	if (c >= 'A' && c <= 'Z')
-	{
-	return 1; // The character is uppercase
-	}
+		return (1);
 	else
-	{
-	return 0; // The character is not uppercase
-	}
-}
-
-int main(void)
-
-{
-	char character = 'A';
-
-	if (_isupper(character))
-	{
-	_putchar(character);
-	_putchar(' ');
-	_putchar('i');
-	_putchar('s');
-	_putchar(' ');
-	_putchar('uppercase');
-	_putchar('\n');
-	}
-	else
-	{
-	_putchar(character);
-	_putchar(' ');
-	_putchar('i');
-	_putchar('s');
-	_putchar(' ');
-	_putchar('not');
-	_putchar(' ');
-	_putchar('uppercase');
-	_putchar('\n');
-	}
-
-	return 0;
+		return (0);
 }

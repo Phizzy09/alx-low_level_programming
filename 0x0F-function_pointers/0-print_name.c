b19e0cfa712e41dcca0dbb5124623440feabba60
Salaudeen Muhammed-Fatiu Akanfe <salaudeenfatiu091@gmail.com>
2023-10-28 16:28:58 -0700

    Fourth commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
+#include "function_pointers.h"
 
-
-#include <stddef.h>
-
-
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-
-#endif
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
